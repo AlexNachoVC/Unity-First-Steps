7a90e097d5fdaedfee218c2fa5dc50bc7f42e00b
Alejandro <alexnachovc12@gmail.com>
2025-03-28 11:01:06 -0600

    otras cosas

--- Assets/Scripts/Choque.cs
+++ Assets/Scripts/Choque.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Choque : MonoBehaviour
 {
+    [SerializeField] ParticleSystem ps;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +13,14 @@ public class Choque : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Ouch!");
+            ps.gameObject.SetActive(true);
+        }
     }
 }
